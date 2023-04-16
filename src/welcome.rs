6d95d63982878e1057834baad6abcd578c745b8a
David Hewitt <davidmhewitt@gmail.com>
2023-04-16 18:44:11 +0100

    Add test example

--- src/welcome.rs
+++ src/welcome.rs
@@ -25,14 +25,13 @@
 
 use gettextrs::gettext;
 use granite::traits::PlaceholderExt;
+use granite::traits::ToastExt;
+use gtk::glib::clone;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{gio, glib};
 
 mod imp {
-    use granite::traits::ToastExt;
-    use gtk::glib::clone;
-
     use super::*;
 
     #[derive(Debug, Default)]
@@ -91,19 +90,20 @@ mod imp {
                 .append_button(
                     &gio::ThemedIcon::new("filter"),
                     &gettext("Custom Icon"),
-                    &gettext("Icon from GResource")
+                    &gettext("Icon from GResource"),
                 )
                 .expect("Unable to construct button in welcome view");
 
+            filter_button.set_widget_name("filter-button");
+
             let toast = granite::Toast::new(&gettext("Filter clicked"));
+            toast.set_widget_name("toast");
 
             filter_button.connect_clicked(clone!(@weak toast => move |_| {
                 toast.send_notification();
             }));
 
-            let overlay = gtk::Overlay::builder()
-                .child(&welcome)
-                .build();
+            let overlay = gtk::Overlay::builder().child(&welcome).build();
             overlay.add_overlay(&toast);
 
             obj.append(&overlay);
@@ -130,5 +130,98 @@ impl WelcomeView {
 impl Default for WelcomeView {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn find_child_widget_by_name(parent: &impl WidgetExt, name: &str) -> Option<gtk::Widget> {
+        let mut child = parent.first_child();
+        while let Some(ref inner) = child {
+            if inner.widget_name() == name {
+                child = Some(inner.clone());
+                break;
+            }
+
+            if let Some(ref recursive) = find_child_widget_by_name(inner, name) {
+                child = Some(recursive.clone());
+                break;
+            }
+
+            child = inner.next_sibling();
+        }
+
+        child
+    }
+
+    fn find_child_widget_by_type<T: IsA<gtk::Widget>>(parent: &impl WidgetExt) -> Option<T> {
+        let mut child = parent.first_child();
+        let mut result: Option<T> = None;
+        while let Some(ref inner) = child {
+            if let Ok(inner) = inner.clone().dynamic_cast::<T>() {
+                result = Some(inner);
+                break;
+            }
+
+            if let Some(ref recursive) = find_child_widget_by_type::<T>(inner) {
+                result = Some(recursive.clone());
+                break;
+            }
+
+            child = inner.next_sibling();
+        }
+
+        result
+    }
+
+    fn wait(ms: u32) {
+        let main_loop = glib::MainLoop::new(None, false);
+        glib::timeout_add(
+            std::time::Duration::from_millis(ms as u64),
+            glib::clone!(@strong main_loop => move || {
+                main_loop.quit();
+                Continue(false)
+            }),
+        );
+
+        main_loop.run();
+    }
+
+    #[gtk::test]
+    fn test_welcome_view() {
+        let window = gtk::Window::new();
+
+        let welcome_view = WelcomeView::new();
+
+        window.set_child(Some(&welcome_view));
+        window.present();
+
+        let filter = find_child_widget_by_name(&welcome_view, "filter-button")
+            .expect("Filter button cannot be found");
+        let filter = filter
+            .dynamic_cast::<gtk::Button>()
+            .expect("Filter button cannot be cast");
+
+        let toast =
+            find_child_widget_by_name(&welcome_view, "toast").expect("Toast cannot be found");
+        let toast = toast
+            .dynamic_cast::<granite::Toast>()
+            .expect("Toast cannot be cast");
+
+        let toast_revealer =
+            find_child_widget_by_type::<gtk::Revealer>(&toast).expect("Can't get toast revealer");
+
+        assert!(!toast_revealer.is_child_revealed());
+
+        filter.activate();
+        wait(1000);
+
+        assert!(toast_revealer.is_child_revealed());
+
+        wait(4000);
+
+        assert!(!toast_revealer.is_child_revealed());
     }
 }
